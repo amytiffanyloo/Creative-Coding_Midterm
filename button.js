b9e9b3b40a37f2c5aa911467217cc43c8761dd24
amytiffanyloo <amytiffanyloo@gmail.com>
2022-10-26 22:36:30 -0400

    Button design
    Created three buttons that act as screen switch

--- button.js
+++ button.js
@@ -2,37 +2,58 @@ class Button {
   constructor(x) {
     this.x = x;
     this.y = 350;
-    this.d = 25;
+    this.r = 25;
     this.click = false;
   }
   show() {
     noStroke();
-    var diam = this.d;
-		if(this.click){
-			diam = this.d * 1.5;
-		}
     fill(100,100,100);
-    ellipse(this.x,this.y,this.d);
-    this.click = false;
+    ellipse(this.x,this.y,this.r);
   }
-
-  
   clicked(){
-    if(this.over()){
-      this.click=true;
+    let d = dist(mouseX,mouseY,this.x,this.y);
+    if(d < this.r)
+    console.log("You clicked!");
+    //sound play 
+  }   
+}
+
+class ButtonLeft extends Button{
+  constructor(x,y,r){
+    super(x,y,r)
+  }
+  
+  triggered(){
+    let d = dist(mouseX,mouseY,this.x,this.y);
+    if(d < this.r){
+      console.log("You clicked on Left button!");
+    mode = 1
     }
-    // this.r = 35
-    
-    //sound play  
-}
+  }
 }
 
-// class ButtonLeft extends Button{
-//   constructor(){
-//     super(x,y,r)
-//   }
-//   trigger(){
-    
-//   }
+class ButtonMiddle extends Button{
+  constructor(x,y,r){
+    super(x,y,r)
+  }
+  
+  triggered(){
+    let d = dist(mouseX,mouseY,this.x,this.y);
+    if(d < this.r)
+    {console.log("You clicked on Middle button!");
+    mode = 2
+  }
+}}
 
-// }
+class ButtonRight extends Button{
+  constructor(x,y,r){
+    super(x,y,r)
+  }
+  
+  triggered(){
+    let d = dist(mouseX,mouseY,this.x,this.y);
+    if(d < this.r)
+    {console.log("You clicked on Right button!");
+    mode = 0
+  }
+}}
--- sketch.js
+++ sketch.js
@@ -1,33 +1,29 @@
 //screen switch reference: https://editor.p5js.org/itsai0724/sketches/B1xmLvw6G
-var mode = 0
-var length= 50
-var shrinkAmount = 2
-var shrink = true 
+var mode = 0;
+var length = 50;
+var shrinkAmount = 2;
+var shrink = true;
 let hungerlevel = 20;
-let tamagotchi
-let button1
-let button2
-let button3
-// let buttonLeft
-// let buttonMiddle
-// let buttonRight
-
+let tamagotchi;
+let buttons;
+let buttonLeft;
+let button2;
+let button3;
 
 function setup() {
   createCanvas(400, 400);
-  background(200,100,170);
+  background(200, 100, 170);
   textSize(60);
   textAlign(CENTER);
-  tamagotchi = new Tamagotchi(); 
-  button1 = new Button(100)
-  button2 = new Button(200)
-  button3 = new Button(300)
-  tamagotchi.preload()
+  tamagotchi = new Tamagotchi();
+  buttonLeft = new ButtonLeft(100);
+  buttonMiddle = new ButtonMiddle(200);
+  buttonRight = new ButtonRight(300);
+  tamagotchi.preload();
 }
 
 function draw() {
   background(220);
-
   if (mode == 0) {
     defaultmode();
   } else if (mode == 1) {
@@ -35,31 +31,30 @@ function draw() {
   } else if (mode == 2) {
     status();
   }
-  
 }
 
 //Useful to have multiple elements on one screen
 function defaultmode() {
-  background(200,100,100)
-  fill(165,170,165);
-  rect(70,60,260,230);
+  background(200, 100, 100);
+  fill(165, 170, 165);
+  rect(70, 60, 260, 230);
   //shape of the buttons
   noStroke();
-  fill(100,100,100);
-  button1.show();
-  button2.show();
-  button3.show();
+  fill(100, 100, 100);
+  buttonLeft.show();
+  buttonMiddle.show();
+  buttonRight.show();
   tamagotchi.show();
 }
 
 function watch() {
-  background(200,100,100)
-  fill(165,170,165);
-  rect(70,60,260,230);
-  fill(100,100,100)
-  button1.show();
-  button2.show();
-  button3.show();
+  background(200, 100, 100);
+  fill(165, 170, 165);
+  rect(70, 60, 260, 230);
+  fill(100, 100, 100);
+  buttonLeft.show();
+  buttonMiddle.show();
+  buttonRight.show();
   hourNum = hour();
   if (minute() < 10) {
     minNum = "0" + minute();
@@ -71,42 +66,29 @@ function watch() {
   } else {
     secNum = second();
   }
-  fill(50)
-  text(hourNum+":"+minNum+":"+secNum, 100,150, width / 2, height / 2);
-
+  fill(50);
+  text(hourNum + ":" + minNum + ":" + secNum, 100, 150, width / 2, height / 2);
 }
 
 function status() {
-  background(200,100,100)
-  fill(165,170,165);
-  rect(70,60,260,230);
-  fill(100,100,100)
-  button1.show();
-  button2.show();
-  button3.show();
-  fill(100,100,200)
-  rect(300,200,shrink,100);
-  shrink = shrink + 1
+  background(200, 100, 100);
+  fill(165, 170, 165);
+  rect(70, 60, 260, 230);
+  fill(100, 100, 100);
+  buttonLeft.show();
+  buttonMiddle.show();
+  buttonRight.show();
+  fill(100, 100, 200);
+  rect(300, 200, shrink, 100);
+  shrink = shrink + 1;
 }
 
 
-function mouseIsPressed(){
-  let d = dist(mouseX, mouseY, button1.x, button1.y);
-    if (d < 30){
-     button1.clicked()
-    } 
-//   button1.clicked();
-//   button2.clicked();
-//   button3.clicked();  
-}
-
-function mouseClicked() {
-
-  if (mode == 0) {
-    mode = 1;
-  } else if (mode == 1) {
-    mode = 2;
-  } else if (mode == 2) {
-    mode = 0;
-  }
+function mousePressed() {
+  buttonLeft.clicked();
+  buttonLeft.triggered();
+  buttonMiddle.clicked();
+  buttonMiddle.triggered();
+  buttonRight.clicked();
+  buttonRight.triggered();
 }
